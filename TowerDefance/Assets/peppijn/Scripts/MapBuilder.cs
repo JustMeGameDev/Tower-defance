9f70a87afc2b13c84c0c06e401cfdb4e9f524ae9
peppie9728 <56298154+peppie9728@users.noreply.github.com>
2022-03-22 15:15:23 +0100

    MapBuilder

--- TowerDefance/Assets/peppijn/Scripts/MapBuilder.cs
+++ TowerDefance/Assets/peppijn/Scripts/MapBuilder.cs
@@ -4,12 +4,48 @@ using UnityEngine;
 
 public class MapBuilder : MonoBehaviour
 {
+    [Header("Tiles")]
     public GameObject pathTile;
     public GameObject grassTile;
     public GameObject EnemySpawn;
     public GameObject EnemyTarget;
+
+    [Header("Basic Map Parameters")]
     public int pathLength;
+    public int mapSize;
+
+    [Header("Seed Generator")]
+    public string stringSeed = "seed string";
+    public bool useStringSeed;
+    public int seed;
+    public bool useRandomSeed;
+
     void Awake()
+    {
+        GenarateSeed();
+        GenarateWorld();
+    }
+
+
+
+
+
+    private void GenarateSeed()
+    {
+        if (useRandomSeed)
+        {
+            seed = Random.Range(0, 99999); 
+        }
+        if (useStringSeed)
+        {
+            seed = stringSeed.GetHashCode();
+        }
+
+        Random.InitState(seed);
+    }
+
+
+    private void GenarateWorld()
     {
 
     }
