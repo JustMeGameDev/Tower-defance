f10b457403219e9cf161929bad1982035908f9b2
JustMeGameDev <kwhartlief@st.noorderpoort.nl>
2022-03-22 14:16:06 +0100

    shop-maneger works

--- TowerDefance/Assets/klaas/Scripts/TowerBluePrint.cs
+++ TowerDefance/Assets/klaas/Scripts/TowerBluePrint.cs
@@ -2,24 +2,4 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TowerBluePrint : MonoBehaviour
-{
-    public GameObject barracks_blueprint;
-    
-    public PopUpZonderCheck Close;
-    
 
-    private void Awake()
-    {
-        Close = GameObject.FindWithTag("ShopMenu").GetComponent<PopUpZonderCheck>();
-      
-    }
-    public void spawn_barracks_blueprint()
-    {
-       
-        Instantiate(barracks_blueprint);
-        Close.IsOpen = false;
-        Close.canvas.enabled = false;
-        Close.Placing = true;
-    }
-}
--- TowerDefance/Assets/kornee/code/ShopMaster.cs
+++ TowerDefance/Assets/kornee/code/ShopMaster.cs
@@ -1,10 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ShopMaster : MonoBehaviour
 {
- 
+    public GameObject Cannon_blueprint;
+    public float priceCannon = 25f;
+    public PopUpZonderCheck Close;
+    public GameMaster gameMaster;
+    public TextMeshProUGUI Message;
 
+    private void Awake()
+    {
+        Close = GameObject.FindWithTag("ShopMenu").GetComponent<PopUpZonderCheck>();
+        gameMaster = GameObject.FindWithTag("GameMaster").GetComponent<GameMaster>();
+
+    }
+    public void spawn_Cannon_blueprint()
+    {
+        if (gameMaster.money >= priceCannon)
+        {
+            Instantiate(Cannon_blueprint);
+            Close.IsOpen = false;
+            Close.canvas.enabled = false;
+            Close.Placing = true;
+            gameMaster.money -= priceCannon;
+        }
+        else
+        {
+            Message.text = "Not Enough Funds";
+        }
+    }
 
 }
+
+
