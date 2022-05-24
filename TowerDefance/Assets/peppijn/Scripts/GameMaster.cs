d4aa18e57a7734a5ce036fa3681e458842a3f548
JustMeGameDev <kwhartlief@st.noorderpoort.nl>
2022-05-24 14:57:33 +0200

    day nightcycle done

--- TowerDefance/Assets/peppijn/Scripts/GameMaster.cs
+++ TowerDefance/Assets/peppijn/Scripts/GameMaster.cs
@@ -73,7 +73,7 @@ public class GameMaster : MonoBehaviour
         {
             moneyText.text = Mathf.Round(money) + "";
         }
-        healthbar.value = PlayerHealth; // Time.deltaTime;
+        healthbar.value = PlayerHealth;
         health.text = PlayerHealth + " / " + PlayerMaxHealth;
         if (PlayerHealth <= 0)
         {
